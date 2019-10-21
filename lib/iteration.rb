281d3096933ea68efe2f4c6f7b3a18b4fb6982dd
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-21 10:41:37 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -22,3 +22,5 @@ def total_even_pairs(src)
   # the number was even. Review the operator documentation if you've forgotten
   # this!
 end
+
+join_ingredients([["peperoni", "mozarella"],["cheese", "olives"]])
