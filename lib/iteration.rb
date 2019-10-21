e2218c8c7db4c8b42f5727b9870d31979cf01b31
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-21 10:43:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -2,7 +2,7 @@ def join_ingredients(src)
 
   row_index = 0
   while row_index < src.count do
-    p "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+    p 1
 
     row_index += 1
   end
